ea93e49ec691a91912a2160e733321c91e443245
Shishir <shishir3324kumar@gmail.com>
2024-10-06 09:01:32 +0530

    minor changes

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,9 +1,15 @@
-export default function Profile() {
-    return (
-        <div className="px-4 py-3 rounded-lg">
-            <div className="flex">
-                <img src="/profile-picture.png" className="w-16" />
-            </div>
+export default function Profile({name}) {
+  return (
+    <div className="px-4 py-3 rounded-lg">
+      <div className="flex">
+        <div>
+          <img src="/Casual-profile.png" className="w-16" />
         </div>
-    )
+        <div>
+            <h5>{name}</h5>
+            <p>Lo</p>
+        </div>
+      </div>
+    </div>
+  );
 }
