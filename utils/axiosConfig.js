148bca591a4626f98a4f0bd8b19862d3676819ac
Shishir <shishir3324kumar@gmail.com>
2024-12-17 13:49:09 +0530

    improved cors policy

--- utils/axiosConfig.js
+++ utils/axiosConfig.js
@@ -5,6 +5,7 @@ export const axiosInstanceWithoutToken = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
     }
 })
 
